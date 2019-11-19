0338358d9d32ef81e90bc41c6d3596f8029ea4b2
Dominic Chu <dominic.m.chu@gmail.com>
2019-11-19 13:35:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -32,8 +32,12 @@ class App < Sinatra::Base
     # "#{whatever}"
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    stringy = ""
+    params.each do |word|
+      stringy << "#{word} "
+    end 
+    "#{stringy}"
+    # "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
     #combines into a single string all the words
 
   end 
